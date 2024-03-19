95675090a7d3ff09b78a3d3cf6ed5b1c96829ba0
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-03-19 23:03:41 +0530

    next js frontend

---
+++ next-js/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Bannere({children}:{
+    children:React.ReactNode
+}){
+return (
+    <>
+    <div className="p-1 ">
+        sdjfasjdfjkasfjsdfkk
+    </div>
+    {children}
+    </>
+)
+}
---
+++ next-js/app/(auth)/signin/page.tsx
@@ -0,0 +1,12 @@
+import Signin from "@/components/Signin";
+
+export default function Signinn(){
+    return (
+        <>
+        <div>
+            <Signin/>
+        </div>
+        </>
+
+    )
+}
