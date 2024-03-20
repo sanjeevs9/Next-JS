2735002afb715dbac700f2381488b6586fb564a5
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-03-20 13:50:35 +0530

    backend-init

---
+++ next-learning-backend/app/loading.tsx
@@ -0,0 +1,15 @@
+
+
+export default function loading(){
+    return (
+        <>
+        <div className="flex flex-col justify-center h-screen">
+            <div className="flex justify-center">
+                Loading ...
+
+            </div>
+
+        </div>
+        </>
+    )
+}
---
+++ next-learning-backend/app/page.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import Image from "next/image";
+
+interface user{
+  email:string,
+  name:string
+}
+
+async function detais(){
+  return await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+    .then(res=>{
+      return res.data;
+    })
+}
+
+export default async function Home() {
+  const data=await detais();
+
+  return (
+    <> 
+    <div className="h-screen flex justify-center items-center bg-red-50">
+
+    
+    <div className="flex h-60 w-60 border-spacing-1
+     justify-center  flex-col items-center border-2" >
+      <div className="flex items-center">
+      {data.email}     
+      </div>
+    <div className="flex items-center">
+    {data.name}
+    </div>
+    
+    </div>
+    </div>
+    </>
+      );
+}
